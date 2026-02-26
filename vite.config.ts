3256cc47ec3c9fc4b01b8892ba8d27900d56d760
Yunseong <dev.yunseong@gmail.com>
2026-02-26 13:11:10 +0900

    fix: update API destination to production URL

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://quantum.yunseong.dev",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
